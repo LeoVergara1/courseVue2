283769c9456ba81dcc9d9234929bda3ce1a82eda
Brandon Vergara <brandon@makingdevs.com>
2018-06-11 22:47:35 -0500

    #3 interpolate variables and methos when these returned a string

--- vue.js
+++ vue.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el:".vue",
-  data: {
-    mensaje: "Aprende Vue.js Fácilmente"
-  }
+    data: {
+      mensaje: "Aprende Vue.js Fácilmente"
+      },
+    methods: {
+      showingMessage: function () {
+        return this.mensaje;
+      }
+    }
 })
