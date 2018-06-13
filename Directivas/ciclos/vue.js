fe8bf3eac2182865a4bb88ace360097573950b7e
Brandon Vergara <brandon@makingdevs.com>
2018-06-13 00:07:05 -0500

    #15 used to for with object and key value

--- Directivas/ciclos/vue.js
+++ Directivas/ciclos/vue.js
@@ -2,6 +2,12 @@ var app = new Vue({
   el:"#vm",
     data: {
       paises: ["México", "España", "Estados Unidos"],
-      paises2: [{nombre: "Mésxico"}, {nombre: "España"}, {nombre: "Estados Unidos"}]
+      paises2: [{nombre: "Mésxico"}, {nombre: "España"}, {nombre: "Estados Unidos"}],
+      empleado: {
+        nombre: "Brandon",
+        edad: 13,
+        nacionalidad: "México",
+        profesion: "Desarrollador"
+      }
     }
 })
