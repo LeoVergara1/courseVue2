e172cef9a5ac1a913a64dfcf92b0924b862fafc6
Brandon Vergara <brandon@makingdevs.com>
2018-06-12 23:54:24 -0500

    #15 used to for with objects

--- Directivas/ciclos/vue.js
+++ Directivas/ciclos/vue.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el:"#vm",
     data: {
-      paises: ["México", "España", "Estados Unidos"]
+      paises: ["México", "España", "Estados Unidos"],
+      paises2: [{nombre: "Mésxico"}, {nombre: "España"}, {nombre: "Estados Unidos"}]
     }
 })
