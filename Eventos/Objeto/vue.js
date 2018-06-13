5f64163d5858f12038daa477b817b542bae4bdf1
Brandon Vergara <brandon@makingdevs.com>
2018-06-13 00:22:50 -0500

    #21 used to event

---
+++ Eventos/Objeto/vue.js
@@ -0,0 +1,13 @@
+new Vue({
+  el:"#vm",
+    data: {
+      x: 0,
+      y: 0
+    },
+    methods: {
+      mostrarUbicacion: function(evento) {
+        this.x = evento.clientX;
+        this.y = evento.clientY;
+      }
+    }
+})
