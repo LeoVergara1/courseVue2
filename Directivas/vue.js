fd7328e101ce9523a5f232eab1f0f9463497d1d6
Brandon Vergara <brandon@makingdevs.com>
2018-06-12 23:35:02 -0500

    #9 used to v-html

--- Directivas/vue.js
+++ Directivas/vue.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el:"#vm",
     data: {
       imagen: 'http://lorempixel.com/300/300',
-      texto: "Hola texto "
+      texto: "Hola texto ",
+      cadena: "<strong> Hola Texto </strong>"
     }
 })
