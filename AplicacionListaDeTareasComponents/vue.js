e1c3f0d8d0ce8db5cd4a2578b5333d840752c3d5
Brandon Vergara <brandon@makingdevs.com>
2018-06-14 00:12:36 -0500

    #39 adding new component

--- AplicacionListaDeTareasComponents/vue.js
+++ AplicacionListaDeTareasComponents/vue.js
@@ -1,3 +1,20 @@
+var data = {
+  tareas:[
+    {
+      texto: "Aprender Vue js",
+      terminada: false
+    },
+    {
+      texto: "Aprender ANgular js",
+      terminada: false
+    },
+    {
+      texto: "Aprender Ionic 2",
+      terminada: false
+    }
+  ],
+  nuevaTarea: ""
+}
  Vue.component('titulo', {
    template: '<h2>{{titulo}}</h2>',
    data: function(){
@@ -5,38 +22,38 @@
    }
  })
 
- const app = new Vue({
-   el:"#app",
-   data: {
-      tareas:[
-        {
-          texto: "Aprender Vue js",
-          terminada: false
-        },
-        {
-          texto: "Aprender ANgular js",
-          terminada: false
-        },
-        {
-          texto: "Aprender Ionic 2",
-          terminada: false
-        }
-      ],
-      nuevaTarea: ""
+ Vue.component('nueva-tarea',{
+   template: `
+   <div class="input-group">
+   <input type="text" @keyup.enter="agregarTarea" placeholder="Escribe una nueva tarea" v-model="nuevaTarea" class="form-control">
+   <span class="input-group-btn">
+   <button type="button" v-on:click="agregarTarea" class="btn btn-primary">Agregar</button>
+   </span>
+   </div>
+   `,
+   data: function() {
+    return data;
    },
    methods: {
-      agregarTarea: function () {
-        let texto = this.nuevaTarea.trim();
-        if(texto){
-          this.tareas.push({
-            texto: texto,
-            terminada: false
-          })
-        }
-        this.nuevaTarea = ""
-      },
-      borrar: function(indice){
-        this.tareas.splice(indice,1)
+    agregarTarea: function () {
+      let texto = this.nuevaTarea.trim();
+      if(texto){
+        this.tareas.push({
+          texto: texto,
+          terminada: false
+        })
+      }
+      this.nuevaTarea = ""
       }
    }
+ })
+ const app = new Vue({
+   el:"#app",
+   data: data,
+   methods: {
+    borrar: function(indice){
+      this.tareas.splice(indice,1)
+    }
+   }
+
  })
